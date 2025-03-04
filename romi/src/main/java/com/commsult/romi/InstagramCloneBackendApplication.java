48f9e4e38b59df19517d82a9770708a6b2852335
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2025-03-04 22:15:03 +0700

    feat: init spring boot project

---
+++ romi/src/main/java/com/commsult/romi/InstagramCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.commsult.romi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneBackendApplication.class, args);
+	}
+
+}
