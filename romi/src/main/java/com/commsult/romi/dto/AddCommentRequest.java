940c2c7e1caa218b4a59b8575b7b6d76ca9f38a2
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2025-03-04 23:09:55 +0700

    feat: add dto

--- romi/src/main/java/com/commsult/romi/dto/AddCommentRequest.java
+++ romi/src/main/java/com/commsult/romi/dto/AddCommentRequest.java
@@ -1,4 +1,10 @@
 package com.commsult.romi.dto;
 
+import lombok.Data;
+
+@Data
 public class AddCommentRequest {
+    private Long postId;
+    private String username;
+    private String text;
 }
--- romi/src/main/java/com/commsult/romi/dto/RegisterRequest.java
+++ romi/src/main/java/com/commsult/romi/dto/RegisterRequest.java
@@ -1,4 +1,10 @@
 package com.commsult.romi.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
 }
